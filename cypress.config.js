5ba9d56686ac81da63b0ce5ec3479458d9562756
mohamed-ali-89 <mohamedaliyousfi89@gmail.com>
2025-12-04 11:30:43 +0100

    add config cypress and yml

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,12 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'mocha-junit-reporter',
+    mochaJunitReporterReporterOptions: {
+      mochaFile: 'reports/junit/test-results-[hash].xml',
+      toConsole: false
+    }
+  }
 });
