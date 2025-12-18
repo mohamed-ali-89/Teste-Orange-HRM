2603ce8357684aaf998eb826b43ac17553c044c7
mohamed-ali-89 <mohamedaliyousfi89@gmail.com>
2025-12-18 10:50:08 +0100

    auth script add URL

--- cypress/e2e/AUTH/AUTH-001.cy.js
+++ cypress/e2e/AUTH/AUTH-001.cy.js
@@ -1,6 +1,6 @@
 describe('AUTH-001 - Authentification OK', () => {
   it('Login avec ID et MDP valides', () => {
-    cy.visit('https://ton-url-orange-hrm');
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
     cy.get('#username').type('admin');
     cy.get('#password').type('admin123');
     cy.get('button[type="submit"]').click();
