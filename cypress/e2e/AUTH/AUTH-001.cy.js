3c7f39317316909344d01b86fc1a3c1dcf9e129d
mohamed-ali-89 <mohamedaliyousfi89@gmail.com>
2025-12-18 10:54:35 +0100

    fixing object name in auth script

--- cypress/e2e/AUTH/AUTH-001.cy.js
+++ cypress/e2e/AUTH/AUTH-001.cy.js
@@ -1,8 +1,8 @@
 describe('AUTH-001 - Authentification OK', () => {
   it('Login avec ID et MDP valides', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    cy.get('#username').type('admin');
-    cy.get('#password').type('admin123');
+    cy.get('input[name="username"]').type('admin');
+    cy.get('input[type="password"]').type('admin123');
     cy.get('button[type="submit"]').click();
     cy.contains('Dashboard').should('be.visible');
   });
